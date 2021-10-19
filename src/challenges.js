3c2ddbc86393a84a30541837bb47c5202ffd9438
Bruce Alberto <brucealberto2@gmail.com>
2021-10-18 21:52:25 -0300

    Requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -55,9 +55,16 @@ function highestCount(array) {
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
 
+  let distancia1 = Math.abs(cat1 - mouse);
+  let distancia2 = Math.abs(cat2 - mouse);
 
-
-
+  if (distancia1 < distancia2) {
+    return "cat1";
+  } else if (distancia1 === distancia2) {
+    return 'os gatos trombam e o rato foge';
+  } else {
+    return "cat2";
+  }
 
 
 
