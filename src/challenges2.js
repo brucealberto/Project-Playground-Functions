9cf94e69ad1fc7fe8a5011ddebc52aae0b7eb9b2
Bruce Alberto <brucealberto2@gmail.com>
2021-10-20 21:41:19 -0300

    Requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,30 @@
 // Desafio 10
-function techList() {
+function techList(tech, name) {
   // seu código aqui
+  tech.sort();
+  let newArray = [];
+  // let tecnologia = {
+
+  // }
+  for (let index = 0; index < tech.length; index += 1) {
+    newArray.push({
+      tech: tech[index],
+      name: name
+    });
+  };
+
+  if (newArray == 0) {
+    return 'Vazio!';
+  }
+  return newArray;
+  // criar um objeto pra cada elemento do array
+  // utilizar o sort() para ordem crescente
+  // objetos tem que serem inseridos em novo array em ordem crescente
+  // tenho que alterar o campo tech do objeto
+  // exemplo {
+  //   tech: "NomeTech"
+  //   name: name
+  // }
 }
 
 // Desafio 11
